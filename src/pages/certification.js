69f49944194f7fd0b331e8fca032c09f46c74b80
lucinda <lucinda.onguyen@gmail.com>
2021-04-18 17:12:57 -0500

    wip

--- src/pages/certification.js
+++ src/pages/certification.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 import Navbar from "../components/navbar";
+import { StaticImage } from "gatsby-plugin-image"
 // markup
 
 function Content({ children }) {
@@ -46,8 +47,37 @@ function TextHeadline({ children }) {
   );
 }
 
-function Headline1({children}) {
-  return (<h1 style ={{fontFamily:'Libre Calson Text, serif', fontStyle:'normal', fontWeight: 'bold', fontSize:'60px', lineHeight:'69px'}}>{children}</h1>)
+function Headline1({ children }) {
+  return (
+    <h1
+      style={{
+        fontFamily: "Libre Calson Text, serif",
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: "60px",
+        lineHeight: "69px",
+        marginBottom:'0px'
+      }}
+    >
+      {children}
+    </h1>
+  );
+}
+
+function BodyCopy({ children }) {
+  return (
+    <p
+      style={{
+        fontStyle: "normal",
+        fontWeight: "400",
+        fontSize: "18px",
+        lineHeight: "28px",
+        marginBottom:'40px'
+      }}
+    >
+      {children}
+    </p>
+  );
 }
 
 function Certification() {
@@ -57,8 +87,28 @@ function Certification() {
       <Content>
         <Row>
           <Column>
-          <TextHeadline><Headline1 >Childen</Headline1></TextHeadline>
-            </Column>
+            <TextHeadline>
+              <Headline1> RAIL Certification</Headline1>
+              <BodyCopy>
+                <strong>
+                  The world’s first independent, accredited certification
+                  program of its kind.
+                </strong>
+                <br />
+                Developed under the Global AI Action Alliance for the World
+                Economic Forum (WEF), along with a diverse community of leading
+                experts, RAIL certification is based on objective assessments of
+                fairness, bias, explainability, and other concrete metrics of
+                responsibly built AI systems. The Schwartz Reisman Institute for
+                Technology and Society (SRI) at University of Toronto is serving
+                as a business partner on the development phase of the
+                initiative.
+              </BodyCopy>
+            </TextHeadline>
+          </Column>
+          <StaticImage src="../images/Badge.png" alt="RAI logo" />
+          <Column>
+          </Column>
         </Row>
       </Content>
       <p>Hello there</p>
